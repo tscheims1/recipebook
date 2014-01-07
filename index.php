d3ac27219a2d7951de61024bdf5ceccd32d9dc17
root <root@lvps62-112-145-48.my-simplyroot.de>
2014-01-07 23:12:28 +0100

    added some testfiles

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 <?php
 
-echo "testfile";
+$handle = pspell_new("de","","","utf-8",PSPELL_NORMAL );
+
+$word = "Gemus";
+
+if(pspell_check($handle,$word))
+{
+	echo "das wort ist gültig";
+}
+else 
+{
+	echo "ungültig";
+	echo "vorschläge";	
+	print_r(pspell_suggest($handle,$word));
+}
 ?>
